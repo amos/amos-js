1209c78fdcfc0f7e21f643fa334e2953db2031f7
Raphael Rafatpanah <raph@anedot.com>
2026-05-20 17:49:24 -0400

    Fix broken vite config

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-import { playwright } from "@vitest/browser-playwright";
 import { defineConfig } from "vite";
 import dts from "vite-plugin-dts";
 
@@ -32,24 +31,5 @@ export default defineConfig({
   },
   esbuild: {
     keepNames: true,
-  },
-  test: {
-    globals: true,
-    browser: {
-      enabled: true,
-      instances: [
-        {
-          browser: "chromium",
-        },
-      ],
-      provider: playwright(),
-      headless: true,
-      screenshotFailures: false,
-    },
-    coverage: {
-      provider: "v8",
-      reporter: ["text", "json", "html"],
-      exclude: ["node_modules/", "dist/", "test/", "vite.config.js"],
-    },
   },
 });
